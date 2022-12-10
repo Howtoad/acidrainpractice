6b07561b81ed64c0d5e54a6283cc7abcc8e5b60e
Kim <howtoad123@gmail.com>
2022-12-10 17:32:42 +0100

    some issues

--- inputTwo.js
+++ inputTwo.js
@@ -1,3 +1,5 @@
+let key1 = sessionStorage.getItem("key1");
+
 function listenForSecondKey() {
   console.log("Select input for '3'");
   // Listen for the next keypress event
@@ -8,6 +10,14 @@ function listenForSecondKey() {
     // Remove the event listener so we don't keep listening
     document.removeEventListener("keypress", listener);
     console.log("Key selected for '3' is: " + event.key);
+
+    // Call the checkSequence function to begin the game
+    if (sessionStorage.getItem(key1) && sessionStorage.getItem(key2)) {
+      checkSequence();
+      const key2Value = sessionStorage.getItem(key2);
+      const inputFor3Element = document.getElementById("inputFor3");
+      inputFor3Element.innerText = key2Value;
+    }
   };
   document.addEventListener("keypress", listener);
 }
