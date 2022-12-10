e5a8cd6262276c0f60c37c301b8205e5aa671599
544146 <wee.tommy99@gmail.com>
2022-12-10 21:45:16 +0000

    Refactor to remove duplicate code

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
+import { useState } from "react";
 import "./App.css";
-import OneInputSelect from "./components/OneInputSelect";
-import ThreeInputSelect from "./components/ThreeInputSelect";
+import CheckSequence from "./components/CheckSequence";
+import InputSelect from "./components/InputSelect";
 
 function App() {
+  const [key1, setKey1] = useState(null);
+  const [key2, setKey2] = useState(null);
+
   return (
     <div className="App">
       <div className="bg-gray-700 w-screen h-screen">
         <h1 className="text-center text-white font-bold text-3xl pt-5">
           Choose your inputs
         </h1>
-        <OneInputSelect></OneInputSelect>
-        <ThreeInputSelect></ThreeInputSelect>
+        <InputSelect keyState={key1} keySetter={setKey1} number={1} />
+        <InputSelect keyState={key2} keySetter={setKey2} number={2} />
+        {key1 && key2 && <CheckSequence key1={key1} key2={key2} />}
       </div>
     </div>
   );
