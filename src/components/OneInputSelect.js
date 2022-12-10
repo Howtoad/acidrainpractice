f051e929d69758f59ddcb9103467ad8738fb0930
Kim <howtoad123@gmail.com>
2022-12-10 21:16:43 +0100

    first set up done

--- src/components/OneInputSelect.js
+++ src/components/OneInputSelect.js
@@ -1,34 +1,18 @@
 import RecorderOne from "./RecorderOne";
 import { useState } from "react";
 
-
-const ListenForFirstKey = () => {
-    console.log("Select input for '1'");
-  
-    // Use the useState hook to create a state variable and a function to update it
-    const [key1, setKey1] = useState(null);
-  
-    const handleKeyPress = (event) => {
-      // Save the key to the state variable
-      setKey1(event.key);
-  
-      // Remove the event listener so we don't keep listening
-      document.removeEventListener("keypress", handleKeyPress);
-      console.log("Key selected for '1' is: " + event.key);
-    };
-  
-    // Add an event listener for the "keypress" event
-    document.addEventListener("keypress", handleKeyPress);
-  
-    return null;
-  };
-
 const OneInputSelect = () => {
+  const [key1, setKey1] = useState(null);
+
   return (
     <div className="flex mb-5">
       <p className="text-white text-2xl">Input Chosen for "1": </p>
-      <p className="text-white text-2xl font-bold pl-2 pr-2 capitalize">K</p>
-      <RecorderOne onClick={ListenForFirstKey}></RecorderOne>
+
+      <p className="text-white text-2xl font-bold pl-2 pr-2 capitalize w-[40px]">
+        {key1}
+      </p>
+
+      <RecorderOne setKey1={setKey1}></RecorderOne>
     </div>
   );
 };
--- src/components/RecorderThree.js
+++ src/components/RecorderThree.js
@@ -1,6 +1,28 @@
-const RecorderThree = () => {
+import React, { useState } from "react";
+
+const RecorderThree = ({ setKey2 }) => {
+  const handleClick = () => {
+    console.log("Select input for '3'");
+    // Add an event listener for the "keypress" event
+    const listener = (event) => {
+      console.log("Key selected for '3' is: " + event.key);
+
+      // Save the key to the state variable
+      setKey2(event.key);
+
+      // Remove the event listener so we don't keep listening
+      document.removeEventListener("keypress", listener);
+    };
+    document.addEventListener("keypress", listener);
+  };
+
+  // Use the useState hook to create a state variable and a function to update it
+
   return (
-    <div className="rounded-full w-8 h-8 bg-red-600 cursor-pointer self-center"></div>
+    <div
+      className="rounded-full w-8 h-8 bg-red-600 cursor-pointer self-center"
+      onClick={handleClick}
+    ></div>
   );
 };
 
