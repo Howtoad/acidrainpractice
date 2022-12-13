87ab16843f0713a7871ddb8a5affd2bfc57097dd
Kim <howtoad123@gmail.com>
2022-12-13 21:24:20 +0100

    fixed the visual prototype bar, and changes to what the console displays (frames instead of time)

--- src/components/CheckSequence.js
+++ src/components/CheckSequence.js
@@ -1,64 +1,70 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 const FRAME_LENGTH = 16.66;
 
 const CheckSequence = ({ frameData }) => {
+  // Convert the time values from frames to milliseconds
+  const frameDataMs = frameData.map((item) => {
+    if (item.length === 1) {
+      return [item[0], 0, Infinity];
+    } else {
+      const msA = item[1] * FRAME_LENGTH;
+      // we add one because this is a range, so we want to include the last frame
+      const msB = (item[2] + 1) * FRAME_LENGTH;
+      return [item[0], msA, msB];
+    }
+  });
 
-    // Convert the time values from frames to milliseconds
-    const frameDataMs = frameData.map((item) => {
-        if (item.length === 1) {
-            return [item[0], 0, Infinity];
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [startTime, setStartTime] = useState(null);
+
+  useEffect(() => {
+    const handleKeyPress = (event) => {
+      // If this is the first key in the sequence, record the start time
+      if (currentIndex === 0) {
+        setStartTime(Date.now());
+      }
+
+      const [key, minTime, maxTime] = frameDataMs[currentIndex];
+
+      // Calculate the time elapsed since the start of the sequence
+      const elapsedTime = Date.now() - startTime;
+      // If the user pressed the correct key and it's within the time range
+      if (
+        event.key === key &&
+        elapsedTime >= minTime &&
+        elapsedTime <= maxTime
+      ) {
+        console.log(
+          `${key}, Framepress: ${Math.round(elapsedTime / 16.66)} minFrame: ${
+            minTime / 16.66
+          }, maxFrame: ${maxTime / 16.66 - 1}`
+        );
+
+        // If we've reached the end of the sequence, print 'true' to the console
+        if (currentIndex === frameDataMs.length - 1) {
+          console.log("Excellent");
+          setCurrentIndex(0);
         } else {
-            const msA = item[1] * FRAME_LENGTH;
-            // we add one because this is a range, so we want to include the last frame
-            const msB = (item[2] + 1) * FRAME_LENGTH;
-            return [item[0], msA, msB];
+          // Move to the next item in the sequence
+          setCurrentIndex(currentIndex + 1);
         }
-    });
+      } else {
+        console.log("failFrame: " + Math.round(elapsedTime / 16.66));
+        console.log("Kamu");
+        setCurrentIndex(0);
+        setStartTime(null);
+      }
+    };
 
-    const [currentIndex, setCurrentIndex] = useState(0);
-    const [startTime, setStartTime] = useState(null);
+    document.addEventListener("keypress", handleKeyPress);
 
-    useEffect(() => {
-        const handleKeyPress = (event) => {
-            // If this is the first key in the sequence, record the start time
-            if (currentIndex === 0) {
-                setStartTime(Date.now());
-            }
+    return () => {
+      document.removeEventListener("keypress", handleKeyPress);
+    };
+  }, [currentIndex, frameDataMs, startTime]);
 
-            const [key, minTime, maxTime] = frameDataMs[currentIndex];
-
-            // Calculate the time elapsed since the start of the sequence
-            const elapsedTime = Date.now() - startTime;
-            // If the user pressed the correct key and it's within the time range
-            if (event.key === key && elapsedTime >= minTime && elapsedTime <= maxTime) {
-                console.log(
-                    `${key}, eventTime: ${elapsedTime} minTime: ${minTime}, maxTime: ${maxTime}`
-                );
-
-                // If we've reached the end of the sequence, print 'true' to the console
-                if (currentIndex === (frameDataMs.length - 1)) {
-                    console.log("Excellent");
-                    setCurrentIndex(0);
-                } else {
-                    // Move to the next item in the sequence
-                    setCurrentIndex(currentIndex + 1);
-                }
-            } else {
-                console.log("Kamu");
-                setCurrentIndex(0);
-                setStartTime(null);
-            }
-        };
-
-        document.addEventListener("keypress", handleKeyPress);
-
-        return () => {
-            document.removeEventListener("keypress", handleKeyPress);
-        };
-    }, [currentIndex, frameDataMs, startTime]);
-
-    return <div>{/* The component doesn't have any rendered content */}</div>;
+  return <div>{/* The component doesn't have any rendered content */}</div>;
 };
 
 export default CheckSequence;
