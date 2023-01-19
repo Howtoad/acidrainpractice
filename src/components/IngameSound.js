5e87a7cc855a5c229999aa158cfca1acc29ff1cd
Kim <howtoad123@gmail.com>
2023-01-19 21:30:06 +0100

    set up for sound

---
+++ src/components/IngameSound.js
@@ -0,0 +1,14 @@
+import { AiFillSound } from "react-icons/ai";
+
+
+const IngameSound = () => {
+  
+
+  return (
+    <div className="cursor-pointer max-w-fit">
+      <AiFillSound  size={64} />
+    </div>
+  );
+};
+
+export default IngameSound;
