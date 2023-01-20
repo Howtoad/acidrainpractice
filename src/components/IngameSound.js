227e9451508b9108b0378a64d2fd4be2930ff60f
Kim <howtoad123@gmail.com>
2023-01-20 10:40:20 +0100

    ingame sound

--- src/components/IngameSound.js
+++ src/components/IngameSound.js
@@ -1,12 +1,30 @@
 import { AiFillSound } from "react-icons/ai";
-
+import ingameSound from "../media/sound/acid_sound.mp3";
+import { useState, useRef, useEffect } from "react";
 
 const IngameSound = () => {
-  
-
+  const [isPlaying, setIsPlaying] = useState(false);
+  const audioRef = useRef(null);
+  useEffect(() => {
+    audioRef.current = new Audio(ingameSound);
+    audioRef.current.loop = true;
+    return () => {
+      audioRef.current.pause();
+    };
+  }, []);
+  const playIngameSound = () => {
+    if (!isPlaying) {
+      audioRef.current.play();
+      setIsPlaying(true);
+    } else {
+      audioRef.current.currentTime = 0;
+      audioRef.current.pause();
+      setIsPlaying(false);
+    }
+  };
   return (
-    <div className="cursor-pointer max-w-fit">
-      <AiFillSound  size={64} />
+    <div className="cursor-pointer max-w-fit text-white">
+      <AiFillSound size={28} onClick={playIngameSound} />
     </div>
   );
 };
